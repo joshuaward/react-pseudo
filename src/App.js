3be32679f149592e6d75109b9a5851298fded13b
Joshua Ward <joshuaward@Joshuas-MacBook-Pro.local>
2023-07-20 19:44:27 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<select multiple className="select-multiple">
+				<option value="option-1">Option 1</option>
+				<option value="option-2">Option 2</option>
+				<option value="option-3">Option 3</option>
+				<option value="option-4">Option 4</option>
+				<option value="option-5">Option 5</option>
+				<option value="option-6">Option 6</option>
+				<option value="option-7">Option 7</option>
+				<option value="option-8">Option 8</option>
+				<option value="option-9">Option 9</option>
+				<option value="option-10">Option 10</option>
+				<option value="option-11">Option 11</option>
+				<option value="option-12">Option 12</option>
+				<option value="option-13">Option 13</option>
+			</select>
+		</div>
   );
 }
 
